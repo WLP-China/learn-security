951a5af80221052800831ad127143af01351ca8e
WLP <503456154@qq.com>
2020-05-14 11:15:24 +0800

    +注释

--- demo02-springSecurity/src/main/java/com/muqing/init/SpringSecurityApplicationInitializer.java
+++ demo02-springSecurity/src/main/java/com/muqing/init/SpringSecurityApplicationInitializer.java
@@ -2,16 +2,16 @@ package com.muqing.init;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+/**
+ * Spring Security初始化
+ */
 public class SpringSecurityApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
     public SpringSecurityApplicationInitializer() {
+        /*
+         * 若当前环境没有使用Spring或Spring MVC，则需要将 WebSecurityConfig(Spring Security配置类) 传入超类，以确保获取配置，并创建spring context。
+         *
+         * 若当前环境已经使用spring，我们应该在现有的springContext中注册Spring Security(SpringApplicationInitializer中已经将WebSecurityConfig加载至rootcontext)，此方法可以什么都不做。
+         * */
         //super(WebSecurityConfig.class);
     }
 }
-
-/*
-Spring Security初始化，这里有两种情况
-若当前环境没有使用Spring或Spring MVC，则需要将 WebSecurityConfig(Spring Security配置类) 传入超类，以确保获取配置，并创建spring context。
-相反，若当前环境已经使用spring，我们应该在现有的springContext中注册Spring Security(上一步已经做将WebSecurityConfig加载至rootcontext)，此方法可以什么都不做。
-
-在init包下定义SpringSecurityApplicationInitializer：
- */
