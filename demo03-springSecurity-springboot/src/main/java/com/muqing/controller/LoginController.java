e2fd213c690b33b402bdf5225f7ed442764c8a68
MuQing <ifunning@163.com>
2020-05-27 14:59:01 +0800

    Spring Security获取当前登录用户信息

--- demo03-springSecurity-springboot/src/main/java/com/muqing/controller/LoginController.java
+++ demo03-springSecurity-springboot/src/main/java/com/muqing/controller/LoginController.java
@@ -1,5 +1,8 @@
 package com.muqing.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,25 +11,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     @PostMapping(value = "/login-success", produces = {"text/plain;charset=UTF-8"})
-    public String loginSuccess(){
-        return " 登录成功";
+    public String loginSuccess() {
+        return getUserName() + " 登录成功";
+    }
+
+    private String getUserName() {
+        //Spring Security获取当前登录用户信息的方法为SecurityContextHolder.getContext().getAuthentication()
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!authentication.isAuthenticated()) {
+            return null;
+        }
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof UserDetails) {
+            return ((UserDetails) principal).getUsername();
+        } else {
+            return principal.toString();
+        }
     }
 
     /**
      * 测试资源1
+     *
      * @return
      */
-    @GetMapping(value = "/r/r1",produces = {"text/plain;charset=UTF-8"})
-    public String r1(){
-        return " 访问资源1";
+    @GetMapping(value = "/r/r1", produces = {"text/plain;charset=UTF-8"})
+    public String r1() {
+        return getUserName() + " 访问资源1";
     }
 
     /**
      * 测试资源2
+     *
      * @return
      */
-    @GetMapping(value = "/r/r2",produces = {"text/plain;charset=UTF-8"})
-    public String r2(){
-        return " 访问资源2";
+    @GetMapping(value = "/r/r2", produces = {"text/plain;charset=UTF-8"})
+    public String r2() {
+        return getUserName() + " 访问资源2";
     }
 }
